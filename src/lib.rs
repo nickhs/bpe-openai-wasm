27476a1a268528833568c216c0248311c8e5ed7b
Nick HS <nick@nickhs.com>
2025-06-14 20:05:26 -0400

    remove boilerplate

--- src/lib.rs
+++ src/lib.rs
@@ -39,18 +39,3 @@ impl Tokenizer {
 pub fn init_panic_hook() {
     console_error_panic_hook::set_once();
 }
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
